28b990330212643c03dfca6fa23db19d8d00b70d
julianitapp <123569438+julianitapp@users.noreply.github.com>
2023-02-08 10:22:39 -0500

    08 febrero

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cardsAdj = [(name: "")];
+  const cardsAdj = [
+    {
+     name:'catnoair' ,
+     img:'imagenes/catnoair.jpg'
+      }
+      {
+      name:'catnoair1' ,
+       img:'imagenes/catnoair1.jpg'
+      }    
+      {
+        name:'ladybug' ,
+        img:'imagenes/ladybug.jpg'
+            } 
+            {
+       name:'ladybug1' ,
+       img:'imagenes/ladybug1.jpg'
+                  } 
+                  {
+       name:'ladybugycatnoair' ,
+       img:'imagenes/ladybugycatnoair.jpg'
+                        } 
+                        {
+       name:'miraculous' ,
+      img:'imagenes/miraculous.jpg'
+                              }   
+
+           {
+       name:'catnoair' ,
+       img:'imagenes/catnoair.jpg'
+          }
+         {
+      name:'catnoair1' ,
+     img:'imagenes/catnoair1.jpg'
+         }    
+          {
+     name:'ladybug' ,
+    img:'imagenes/ladybug.jpg'
+          } 
+           {
+     name:'ladybug1' ,
+    img:'imagenes/ladybug1.jpg'
+      } 
+      {
+      name:'ladybugycatnoair' ,
+      img:'imagenes/ladybugycatnoair.jpg'
+       } 
+     {
+     name:'miraculous' ,
+    img:'imagenes/miraculous.jpg'
+      }  
+         
+
+      
 });
