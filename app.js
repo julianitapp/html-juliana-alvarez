3d1e2ac1b4d678bd22427fe0732fae1a20395c6d
julianitapp <123569438+julianitapp@users.noreply.github.com>
2023-04-12 10:37:03 -0500

    12/04/2023

--- app.js
+++ app.js
@@ -56,19 +56,46 @@ document.addEventListener("DOMContentLoaded", () => {
   var cartasEscogidasId = [];
   var cartasGanadas = [];
 
-  //----------------- lecture_03 ----------------------------------//
   function crearTablero() {
     for (let i = 0; i < cardAdj.length; i++) {
-      // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
-      // cuando i=cardAdj.length, es decir, 12.
-      var carta = document.createElement("img"); // Crea elemento img cada vez que se ejecuta la función.
-      carta.setAttribute("src", "imagenes/reverso.png"); //Asignar nuevo atributo a cada carta
-      //igual a la imagen 'reverso'.
-      carta.setAttribute("data-id", i); //Asignar como atributo a cada carta creada, el id=i.
-      carta.addEventListener("click", voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta
-      //la función voltearCarta.
-      cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
-      // cuadricula de html para que se imprima la carta creada en el div de html.
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "imagenes/reverso.png");
+
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+
+      cuadricula.appendChild(carta);
+    }
+  }
+
+  function verificarPareja() {
+    var cards = document.querySelectorAll("img");
+    const opcionUnoId = cartasEscogidasId[0];
+    const opcionDosId = cartasEscogidasId[1];
+
+    if (opcionUnoId === opcionDosId) {
+      cards[opcionUnoId].setAttribute("src", "imagenes/reverso.png");
+      cards[opcionDosId].setAttribute("src", "imagenes/reverso.png");
+      alert("¡diste click a la misma imagen!");
+    } else if (cartasEscogidas[0] === cartasEscogidas[1]) {
+      alert("¡Correcto!");
+      cards[opcionUnoId].setAttribute("src", "imagenes/blanck.png");
+      cards[opcionDosId].setAttribute("src", "imagenes/blank.png");
+      cards[opcionUnoId].removeEventListener("click", voltearCarta);
+      cards[opcionDosId].removeEventListener("click", voltearCarta);
+      cartasGanadas.push(cartasEscogidas);
+    } else {
+      cards[opcionUnoId].setAttribute("src", "imagenes/reverso.png");
+      cards[opcionDosId].setAttribute("src", "imagenes/reverso.png");
+      alert("¡Intenta de nuevo!");
+    }
+    cartasEscogidas = [];
+    cartasEscogidasId = [];
+
+    resultado.textContent = cartasGanadas.length;
+
+    if (cartasGanadas.length === cardAdj.length / 2) {
+      resultado.textContent = "¡felicidades, encontraste todos los pares!";
     }
   }
 
